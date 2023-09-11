70f1b6305d46dbe12382d4f1b1ac53dbee1cdba9
NattapolSurinkeaw <nattapol.surinkeaw@gmail.com>
2023-09-11 09:03:52 +0700

    add_for _pull

--- resources/views/backend/pages/backend_pending_coursedetail.blade.php
+++ resources/views/backend/pages/backend_pending_coursedetail.blade.php
@@ -8,48 +8,48 @@
     </div>
     <div class="flex flex-col gap-4 mx-10 bg-white p-4 rounded-lg drop-shadow-lg">
       <div class="flex gap-4 items-center">
-        <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
-        <input type="text" value="{{$pendingcourse->name}}" name="username" id="username" class="block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
+        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+        <input type="text" value="{{$pendingcourse->name}}" name="name" id="name" class="block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
       </div>
 
       <div class="flex gap-4">
-        <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Course</label>
+        <label for="course" class="block text-sm font-medium leading-6 text-gray-900">Course</label>
         <div class="flex flex-col gap-2 w-full">
         @foreach($courses as $course)
-          <input type="text" value="{{$course->course_name}}"  name="username" id="username" autocomplete="username" class="w-full block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith" disabled>
+          <input type="text" value="{{$course->course_name}}"  name="course" id="course" class="w-full block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="janesmith" disabled>
         @endforeach
         </div>
       </div>
 
       <div class="grid grid-cols-2 gap-3">
         <div class="flex gap-4 items-center">
-          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">เบอร์โทรผู้ซื้อ</label>
-          <input type="text" value="{{$pendingcourse->customer_tel}}" name="username" id="username" class="block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
+          <label for="customer-tel" class="block text-sm font-medium leading-6 text-gray-900">เบอร์โทรผู้ซื้อ</label>
+          <input type="text" value="{{$pendingcourse->customer_tel}}" name="customer-tel" id="customer-tel" class="block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
         </div>
   
         <div class="flex gap-4 items-center">
-          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">จำนวนที่โอน</label>
-          <input type="text" value="{{$pendingcourse->totalmoney}}" name="username" id="username" class="block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
+          <label for="total-money" class="block text-sm font-medium leading-6 text-gray-900">จำนวนที่โอน</label>
+          <input type="text" value="{{$pendingcourse->totalmoney}}" name="total-money" id="total-money" class="block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
         </div>
   
         <div class="flex gap-4 items-center">
-          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">ราคาทั้งหมด</label>
-          <input type="text" value="{{$pendingcourse->total_price}}" name="username" id="username" class="block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
+          <label for="total-price" class="block text-sm font-medium leading-6 text-gray-900">ราคาทั้งหมด</label>
+          <input type="text" value="{{$pendingcourse->total_price}}" name="total-price" id="total-price" class="block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
         </div>
   
         <div class="flex gap-4 items-center">
-          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">เลขบัญชีผู้ซื้อ</label>
-          <input type="text" value="{{$pendingcourse->customer_banknumber}}" name="username" id="username" class="block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
+          <label for="customer-banknumber" class="block text-sm font-medium leading-6 text-gray-900">เลขบัญชีผู้ซื้อ</label>
+          <input type="text" value="{{$pendingcourse->customer_banknumber}}" name="customer-banknumber" id="customer-banknumber" class="block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
         </div>
   
         <div class="flex gap-4 items-center">
-          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">ธนาคารผู้ซื้อ</label>
-          <input type="text" value="{{$pendingcourse->bankcustomer}}" name="username" id="username" class="block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
+          <label for="bank-customer" class="block text-sm font-medium leading-6 text-gray-900">ธนาคารผู้ซื้อ</label>
+          <input type="text" value="{{$pendingcourse->bankcustomer}}" name="bank-customer" id="bank-customer" class="block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
         </div>
   
         <div class="flex gap-4 items-center">
-          <label for="username" class="block text-sm font-medium leading-6 text-gray-900">ธนาคารบริษัท</label>
-          <input type="text" value="{{$pendingcourse->bankcompany}}" name="username" id="username" class="block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
+          <label for="bank-company" class="block text-sm font-medium leading-6 text-gray-900">ธนาคารบริษัท</label>
+          <input type="text" value="{{$pendingcourse->bankcompany}}" name="bank-company" id="bank-company" class="block flex-1 border rounded-md bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
         </div>
   
         <div class="flex gap-4">
@@ -74,8 +74,8 @@
 
     <div class="mx-10 mt-4 bg-white p-4 rounded-lg drop-shadow-lg">
       <div class="flex flex-col">
-        <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Comment</label>
-        <textarea id="about" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+        <label for="comment" class="block text-sm font-medium leading-6 text-gray-900">Comment</label>
+        <textarea id="comment" name="comment" rows="3" class="block p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
       </div>
     </div>
     <div class="mx-10">
@@ -90,7 +90,7 @@
 <script>
 
 let statusSelect = document.querySelector('#status');
-let username = document.querySelector('#username').value;
+let username = document.querySelector('#name').value;
 
 window.onload = function() {
   var selectedOption = statusSelect.options[statusSelect.selectedIndex];
@@ -137,9 +137,10 @@ statusSelect.addEventListener('change', function() {
 });
 
 let pending = {!! json_encode($pendingcourse) !!}
-console.log(pending);
-console.log(pending.total_courses);
+console.log(pending.user_id);
+// console.log(pending.total_courses);
 function approvePending() {
+  let comment = document.querySelector('#comment').value;
   let status = statusSelect.value
   let course = pending.total_courses
   let userId = pending.user_id
@@ -147,57 +148,10 @@ function approvePending() {
     status: status,
     course: course,
     userId: userId,
+    comment: comment,
   }
-  console.log(param)
+  console.log(param);
 }
-// document.getElementById('toggleTable').addEventListener('click', function() {
-//     let tableDiv = document.getElementById('table-train');
-//     let calendarDiv = document.getElementById('calendar-train');
-//     if (tableDiv.style.display === 'none' || tableDiv.style.display === '') {
-//         tableDiv.style.display = 'block';
-//         calendarDiv.style.display = 'none';
-//     }
-// });
-
-// document.getElementById('toggleCalendar').addEventListener('click', function() {
-//     let calendarDiv = document.getElementById('calendar-train');
-//     let tableDiv = document.getElementById('table-train');
-//     if (calendarDiv.style.display === 'none' || calendarDiv.style.display === '') {
-//         calendarDiv.style.display = 'block';
-//         tableDiv.style.display = 'none';
-//     }
-//     let calendarEl = document.querySelector('#calendar');
-//     let calendar = new FullCalendar.Calendar(calendarEl, {
-//         // กำหนดค่าต่าง ๆ ของปฏิทินที่นี่
-//     });
-//     calendar.render();
-// });
-
-
-// function delTraining(train_id) {
-//     Swal.fire({
-//     title: 'Are you sure?',
-//     text: "You won't be able to revert this!",
-//     icon: 'warning',
-//     showCancelButton: true,
-//     confirmButtonColor: '#3085d6',
-//     cancelButtonColor: '#d33',
-//     confirmButtonText: 'Yes, delete it!'
-//     }).then((result) => {
-//     if (result.isConfirmed) {
-//         axios.delete(`/api/backend/deltrain/${train_id}`).then((result) => {
-//             console.log(result.status);
-//             if(result.status = 'success') {
-//                 Swal.fire(
-//                 'Deleted!',
-//                 'Your file has been deleted.',
-//                 'success'
-//                 ).then(() => { location.reload();})
-//             }
-//         })
-//     }
-//     })
-// }
 
 </script>
 
