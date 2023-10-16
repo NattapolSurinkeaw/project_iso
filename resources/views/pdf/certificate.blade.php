f6e1a524e9dc3e90dd99df68d5b380fd830ce9fd
Nattapol <nattapol.surinkeaw@gmail.com>
2023-10-16 17:19:19 +0700

    b_update_fonts

--- resources/views/pdf/certificate.blade.php
+++ resources/views/pdf/certificate.blade.php
@@ -2,13 +2,38 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8-general-ci"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <style>
-    body { 
+    @font-face {
+            font-family: 'THSarabunNew';
+            font-style: normal;
+            font-weight: normal;
+            src: url("{{ public_path('fonts/THSarabunNew.ttf') }}") format('truetype');
+        }
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: normal;
+            font-weight: bold;
+            src: url("{{ public_path('fonts/THSarabunNew Bold.ttf') }}") format('truetype');
+        }
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: italic;
+            font-weight: normal;
+            src: url("{{ public_path('fonts/THSarabunNew Italic.ttf') }}") format('truetype');
+        }
+        @font-face {
+            font-family: 'THSarabunNew';
+            font-style: italic;
+            font-weight: bold;
+            src: url("{{ public_path('fonts/THSarabunNew BoldItalic.ttf') }}") format('truetype');
+        }
+        body { 
+        font-family: "THSarabunNew";
         background-image: url('image/background/border-certificate.jpg'); /* ใช้รูปภาพเป็นพื้นหลัง */
         background-size: 100% 100%; /* ปรับขนาดรูปภาพให้เต็มหน้าจอ */
         background-origin: content-box; /* ไม่รวมขอบขาวที่ปรากฏ */
@@ -56,7 +81,7 @@
         <h3 style="margin-top: 70px;">for outstanding performance in</h3>
         <h3 style="margin-top: 40px;">ELEARNING COURSE</h3>
     </div>
-
+    <h1>สวัสดีครับ</h1>
     {{-- <h1>Page 1</h1>
     <div class="page-break"></div>
     <h1>Page 2</h1> --}}
